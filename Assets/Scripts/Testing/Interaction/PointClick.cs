09ceef691cd289ed35d52be670c00bc3ef172283
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2021-11-17 15:37:09 +0100

    Added text diff feature
    Added diff-match-patch library from https://github.com/google/diff-match-patch.
    
    Added public diff_lineMode method to library according to instructions in https://github.com/google/diff-match-patch/wiki/Line-or-Word-Diffs.
    
    Tested diff feature via PointClick.cs component.

--- Assets/Scripts/Testing/Interaction/PointClick.cs
+++ Assets/Scripts/Testing/Interaction/PointClick.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using DiffMatchPatch;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,9 +11,9 @@ public class PointClick : MonoBehaviour, IPointerClickHandler {
     public GameObject testObject;
 
     public void OnPointerClick(PointerEventData eventData) {
-        var configManager = testObject.GetComponent<ConfigManager>();
-        configManager.AddConfig(configManager.Config1);
         Debug.Log("PointClick Component was clicked!");
+
+        StuffDoneWhenClicked();
     }
 
     // Start is called before the first frame update
@@ -22,5 +24,27 @@ public class PointClick : MonoBehaviour, IPointerClickHandler {
     // Update is called once per frame
     void Update() {
 
+    }
+
+    private void StuffDoneWhenClicked() {
+        diff_match_patch dmp = new diff_match_patch();
+
+        List<Diff> diff = dmp.diff_lineMode(
+            "I am the very model of a modern Major-General,"+ Environment.NewLine +
+            "I've information vegetable, animal, and mineral," + Environment.NewLine +
+            "I know the kings of England, and I quote the fights historical," + Environment.NewLine +
+            "From Marathon to Waterloo, in order categorical.",
+
+            "I am the very model of a modern Major-General," + Environment.NewLine +
+            "I know the kings of England, and I quote the fights historical," + Environment.NewLine +
+            "Lari faur krum ipsum asfk fhe apeghfg dbisf  aufg" + Environment.NewLine +
+            "From Marathon to Waterloo, in order categorical."
+            );
+        // Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
+        dmp.diff_cleanupSemantic(diff);
+        // Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
+        for (int i = 0; i < diff.Count; i++) {
+            Debug.Log(diff[i]);
+        }
     }
 }
