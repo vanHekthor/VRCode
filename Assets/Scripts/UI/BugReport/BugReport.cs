7fb27fe8145753bf29766652278bfad4229b3dd0
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2022-07-18 14:18:42 +0200

    Fix bug report
    remove white box when toggling Code Window Mode Overview

---
+++ Assets/Scripts/UI/BugReport/BugReport.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugReport : MonoBehaviour {
+    public void SetVisibility(bool visible) {
+        gameObject.SetActive(visible);
+    }
+}
