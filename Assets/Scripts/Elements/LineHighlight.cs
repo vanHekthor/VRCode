d56705eee85da34708abc84ac168712889271ac9
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2022-08-08 10:22:02 +0200

    Add ChangeColor method

--- Assets/Scripts/Elements/LineHighlight.cs
+++ Assets/Scripts/Elements/LineHighlight.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace VRVis.Elements {
 
     /// <summary>
     /// Manages the a highlighted code region. Is attached to every line highlight object but does nothing at the moment.
     /// </summary>
+    [RequireComponent(typeof(Image))]
     public class LineHighlight : MonoBehaviour {
-        // Start is called before the first frame update
-        void Start() {
+        private Image background;
 
+        void Start() {
+            background = transform.GetComponent<Image>();
         }
 
-        // Update is called once per frame
-        void Update() {
-
+        public void ChangeColor(Color color) {
+            background.color = color;
         }
     }
 }
